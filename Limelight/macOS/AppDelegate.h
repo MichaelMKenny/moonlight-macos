da0460fb2910470d7efdea1e8d7ea48fdc97ac9e
Michael Kenny <git@mmkenny.net>
2017-12-22 15:45:50 +1030

    Created new macOS target.
    Rearranged project hierachy to accommodate both platforms.

---
+++ Limelight/macOS/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Moonlight for macOS
+//
+//  Created by Michael Kenny on 22/12/17.
+//  Copyright © 2017 Moonlight Stream. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Limelight/macOS/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  Moonlight for macOS
+//
+//  Created by Michael Kenny on 22/12/17.
+//  Copyright © 2017 Moonlight Stream. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
