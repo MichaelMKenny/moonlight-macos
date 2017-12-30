3ab95405ebd5ae3ba2f3be019caba91aa4f74e1a
Michael Kenny <git@mmkenny.net>
2017-12-30 13:25:58 +1030

    Started work on cells growing on hover.

---
+++ Limelight/macOS/Views/AppCellView.m
@@ -0,0 +1,40 @@
+//
+//  AppCellView.m
+//  Moonlight for macOS
+//
+//  Created by Michael Kenny on 30/12/17.
+//  Copyright © 2017 Moonlight Stream. All rights reserved.
+//
+
+#import "AppCellView.h"
+
+@interface AppCellView ()
+@property (nonatomic, strong) NSTrackingArea *trackingArea;
+
+@end
+
+@implementation AppCellView
+
+- (void)viewDidMoveToWindow {
+    [super viewDidMoveToWindow];
+}
+
+- (void)updateTrackingAreas {
+    if (self.trackingArea != nil) {
+        [self removeTrackingArea:self.trackingArea];
+    }
+
+    int opts = (NSTrackingMouseEnteredAndExited | NSTrackingActiveAlways);
+    self.trackingArea = [ [NSTrackingArea alloc] initWithRect:[self bounds] options:opts owner:self userInfo:nil];
+    [self addTrackingArea:self.trackingArea];
+}
+
+//- (void)mouseEntered:(NSEvent *)event {
+//    self.layer.transform = CATransform3DMakeScale(1.1, 1.1, 1);
+//}
+//
+//- (void)mouseExited:(NSEvent *)event {
+//    self.layer.transform = CATransform3DIdentity;
+//}
+
+@end
