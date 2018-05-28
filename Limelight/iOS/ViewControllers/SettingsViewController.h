035d8863e6a67ffb4784e6616e6d64ad1d753d01
Cameron Gutman <aicommander@gmail.com>
2018-05-28 11:39:04 -0700

    Allow the settings view to scroll on smaller iPhones
    # Conflicts:
    #	Limelight/iOS/ViewControllers/SettingsViewController.h

--- Limelight/iOS/ViewControllers/SettingsViewController.h
+++ Limelight/iOS/ViewControllers/SettingsViewController.h
@@ -14,6 +14,7 @@
 @property (strong, nonatomic) IBOutlet UISegmentedControl *framerateSelector;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *resolutionSelector;
 @property (strong, nonatomic) IBOutlet UISegmentedControl *onscreenControlSelector;
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
 @property (weak, nonatomic) IBOutlet UISegmentedControl *optimizeGameSettingsSelector;
 @property (weak, nonatomic) IBOutlet NSLayoutConstraint *leadingContentViewConstraint;
 
