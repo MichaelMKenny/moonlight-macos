87f4ff38943b731d09fb5734cbde693e69e89f58
Michael Kenny <git@mmkenny.net>
2020-09-23 12:40:37 +0930

    Updated ResolutionSyncRequester with support for setting mouse pointer speed.

--- Limelight/macOS/Helpers/ResolutionSyncRequester.swift
+++ Limelight/macOS/Helpers/ResolutionSyncRequester.swift
@@ -18,6 +18,7 @@ class ResolutionSyncRequester: NSObject {
             Self.disableMouseAcceleration(for: host)
         }
         
+        Self.setMouseSpeed(for: host, speed: 12)
         Self.setScrollLines(for: host, lines: 1)
 
         let enabled = UserDefaults.standard.bool(forKey: "shouldSync")
@@ -45,6 +46,10 @@ class ResolutionSyncRequester: NSObject {
 
     static private func disableMouseAcceleration(for host: String) {
         ResolutionSyncRequester.makeRequest(URL(string: "http://\(host):\(port)/resolutionsync/disableMouseAcceleration")!)
+    }
+
+    static private func setMouseSpeed(for host: String, speed: Int) {
+        ResolutionSyncRequester.makeRequest(URL(string: "http://\(host):\(port)/resolutionsync/setMouseSpeed?\(speed)")!)
     }
 
     static private func setScrollLines(for host: String, lines: Int) {
