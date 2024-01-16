577cc3d25ba2895a31522bea3a493e56c54542f8
Michael Kenny <git@mmkenny.net>
2024-01-16 15:43:12 +1030

    Fancier pane cells, with icons and background colors.

--- Limelight/macOS/ViewControllers/SettingsView.swift
+++ Limelight/macOS/ViewControllers/SettingsView.swift
@@ -8,32 +8,39 @@
 
 import SwiftUI
 
+struct PaneCell: Hashable {
+    let title: String
+    let symbol: String
+    let color: Color
+}
+
+let panes: [PaneCell] = [
+    PaneCell(title: "Stream", symbol: "airplayvideo", color: .blue),
+    PaneCell(title: "Video and Audio", symbol: "video.fill", color: .orange),
+    PaneCell(title: "Input", symbol: "keyboard.fill", color: .purple),
+    PaneCell(title: "App", symbol: "appclip", color: .pink)
+]
+
 @available(macOS 13.0, *)
 struct SettingsView: View {
-    @State private var selectedPane = "General"
-    let panes = [
-        "General",
-        "Test"
-    ]
+    @State private var selectedPane = panes.first!
     
     var body: some View {
         NavigationSplitView {
             List(selection: $selectedPane) {
                 ForEach(panes, id: \.self) { pane in
-                    Text(pane)
+                    PaneCellView(paneCell: pane)
                 }
             }
             .listStyle(.sidebar)
-            .navigationSplitViewColumnWidth(150)
+            .navigationSplitViewColumnWidth(160)
         } detail: {
             VStack {
-                if selectedPane == "General" {
-                    GeneralView()
-                } else if selectedPane == "Test" {
-                    TestView()
+                if selectedPane.title == "Stream" {
+                    StreamView()
                 }
             }
-            .navigationSubtitle(selectedPane)
+            .navigationSubtitle(selectedPane.title)
             .toolbarRole(.editor)
             .toolbar {
                 ToolbarItem(placement: .navigation) {
@@ -48,9 +55,31 @@ struct SettingsView: View {
     }
 }
 
-struct GeneralView: View {
+struct PaneCellView: View {
+    let paneCell: PaneCell
+    
     var body: some View {
-        Text("General")
+        let iconSize = CGFloat(14)
+        let containerSize = iconSize + (iconSize / 3)
+
+        HStack(spacing: 6) {
+            Image(systemName: paneCell.symbol)
+                .font(.callout)
+                .frame(width: containerSize, height: containerSize)
+                .padding(1)
+                .background(
+                    RoundedRectangle(cornerRadius: 5, style: .continuous)
+                        .foregroundColor(paneCell.color)
+                )
+
+            Text(paneCell.title)
+        }
+    }
+}
+
+struct StreamView: View {
+    var body: some View {
+        Text("Stream")
     }
 }
 
